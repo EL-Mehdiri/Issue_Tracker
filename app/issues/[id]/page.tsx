54add8da94e2797391d78f876dd388556d0a0073
Mehdi Ridmani <117091960+EL-Mehdiri@users.noreply.github.com>
2023-10-24 21:05:00 +0100

    add additional loading skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -8,15 +8,12 @@ interface Props {
     params: { id: string }
 }
 const IssueDetailPage = async ({ params: { id } }: Props) => {
-    // if (typeof id !== 'number') notFound()
-
     const issue = await prisma.issue.findUnique({
         where: { id: parseInt(id) }
     })
 
     if (!issue)
         notFound()
-
     return (
         <div>
             <Heading>{issue.title}</Heading>
