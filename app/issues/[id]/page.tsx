f56d829c518dcf208cbf6714ab01c2be9925c7de
Mehdi Ridmani <117091960+EL-Mehdiri@users.noreply.github.com>
2023-10-27 08:41:21 +0100

    adding Delete Button and deal with cash in next js

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,9 @@
 import prisma from '@/prisma/client'
-import { Box, Grid } from '@radix-ui/themes'
+import { Box, Flex, Grid } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import EditIssueButton from './EditIssueButton'
 import IssueDetails from './IssueDetails'
+import DeleteIssueButton from './DeleteIssueButton'
 
 interface Props {
     params: { id: string }
@@ -15,12 +16,15 @@ const IssueDetailPage = async ({ params: { id } }: Props) => {
     if (!issue)
         notFound()
     return (
-        <Grid columns={{ initial: '1', md: '2' }} gap={'5'}>
-            <Box>
+        <Grid columns={{ initial: '1', md: '5' }} gap={'5'} >
+            <Box className='col-span-4'>
                 <IssueDetails issue={issue} />
             </Box>
             <Box>
-                <EditIssueButton issueId={id} />
+                <Flex gap={'4'} direction={'column'}>
+                    <EditIssueButton issueId={id} />
+                    <DeleteIssueButton issueId={id} />
+                </Flex>
             </Box>
         </Grid>
     )
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -43,6 +43,5 @@ const Issues = async () => {
     )
 }
 export const dynamic = 'force-dynamic'
-// export const revalidate = 0
 
 export default Issues
