a989def678e172b1bed31dca6cfccf597a44376b
Mehdi Ridmani <117091960+EL-Mehdiri@users.noreply.github.com>
2023-10-28 19:52:55 +0100

    add login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,12 +4,15 @@ import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
 import { usePathname } from 'next/navigation'
 import classNames from 'classnames'
+import { Box } from '@radix-ui/themes'
+import { useSession } from 'next-auth/react'
 
 const NavBar = () => {
     const currentPath = usePathname()
+    const { status, data: session } = useSession()
     const links = [
         { lable: 'Dashboard', href: '/' },
-        { lable: 'Issues', href: '/issues/list' }
+        { lable: 'Issues', href: '/issues/list' },
     ]
     return (
         <nav className='flex space-x-6 items-center  border-b mb-5 px-5 h-14'>
@@ -27,6 +30,12 @@ const NavBar = () => {
                 ))}
 
             </ul>
+            <Box>
+                {status === 'authenticated' &&
+                    <Link href='/api/auth/signout'>Log Out</Link>}
+                {status === 'unauthenticated' &&
+                    <Link href='/api/auth/signin'>Login</Link>}
+            </Box>
         </nav>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import '@radix-ui/themes/styles.css';
 import { Container, Theme, ThemePanel } from '@radix-ui/themes';
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
+import AuthProvider from './auth/Provider';
 
 
 const inter = Inter({
@@ -25,15 +26,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme appearance="light" accentColor="violet">
-          <NavBar />
-          <main className='p-5'>
-            <Container>
-              {children}
-            </Container>
-          </main>
+        <AuthProvider>
+          <Theme appearance="light" accentColor="violet">
+            <NavBar />
+            <main className='p-5'>
+              <Container>
+                {children}
+              </Container>
+            </main>
 
-        </Theme>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   )
