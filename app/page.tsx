fbac9f58b4a99ecbc4b454b836273559df39ae63
Mehdi Ridmani <117091960+EL-Mehdiri@users.noreply.github.com>
2023-11-05 13:12:23 +0100

    the latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import Pagination from './components/Pagination'
 
-export default function Home({ searchParams }: { searchParams: { page: string } }) {
+import LatestIssues from './LatestIssues'
+
+export default function Home() {
   return (
     <div >
-      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
+      <LatestIssues />
     </div>
   )
 }
